e7fc5efa9dbc71d37f167e64915979807496b5d4
ahmedzahran24 <ahmedzahran222111@gmail.com>
2023-03-11 11:57:19 +0200

    edite on home page

--- lib/course.dart
+++ lib/course.dart
@@ -12,27 +12,16 @@ class Course {
   });
 }
 
-final List<Course> courses = [
-  Course(
-    title: "Elevator problems",
-  ),
-  Course(
-    title: "Animations in Flutter",
-    iconSrc: "assets/icons/code.svg",
-    color: const Color(0xFF80A4FF),
-  ),
-];
-
 final List<Course> recentCourses = [
-  Course(title: "State Machine"),
+  Course(title: "amar"),
   Course(
-    title: "Animated Menu",
+    title: "adel",
     color: const Color(0xFF9CC5FF),
     iconSrc: "assets/icons/code.svg",
   ),
-  Course(title: "Flutter with Rive"),
+  Course(title: "zahran"),
   Course(
-    title: "Animated Menu",
+    title: "rashad",
     color: const Color(0xFF9CC5FF),
     iconSrc: "assets/icons/code.svg",
   ),
