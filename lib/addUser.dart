4f2843532e4852b5b1fac08b732da07f6e538e61
Ahmedx2410 <80593168+Ahmedx2410@users.noreply.github.com>
2023-03-23 21:48:21 +0200

    user adding page for admin while counter is stored in the fire store

--- lib/addUser.dart
+++ lib/addUser.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:test2/homePage.dart';
 
 //hello 2
