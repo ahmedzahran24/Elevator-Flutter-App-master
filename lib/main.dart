bf33ff30cc0d6e5661a84fd133df9c2391865e3f
ahmedzahran24 <ahmedzahran222111@gmail.com>
2023-05-13 13:23:20 +0300

    تظبيط

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,8 @@ import 'package:test2/entry_point.dart';
 import 'package:test2/homePage.dart';
 import 'package:test2/home_screen.dart';
 import 'package:test2/login/login.dart';
-<<<<<<< HEAD
-
 import 'package:test2/satat%20user.dart';
 
-=======
->>>>>>> 1d2f2a6e674a0ef7df6d3f6c960bea9d6e120146
 import 'package:test2/start.dart';
 import 'package:test2/team/screens/team_screen.dart';
 import 'package:test2/unlock.dart';
